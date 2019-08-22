ff74097007e04c421de12811673ee690d58cb4bb
PURPLE\Rajamohan S <rajamohan.s@emishealth.com>
2019-08-22 08:01:20 +0100

    First python

---
+++ First program.py
@@ -0,0 +1,12 @@
+def prod (num1, num2):
+    value = num1 * num2
+    if(value<1000):
+        return value
+    else:
+        return num1+num2
+
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+
+result = prod(number1, number2)
+print ("Result is: ", result)
